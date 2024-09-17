18ee37767394e46c40a3102941f1e48db398bdef
avar-gith <holub.norbert@gmail.com>
2024-09-17 02:09:48 +0200

    added Model settings

--- adan/admin.py
+++ adan/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Model, Type, Personality, LearningPath
 
-# Register your models here.
+# Egyedi admin osztály a Model adminisztrációhoz
+@admin.register(Model)
+class ModelAdmin(admin.ModelAdmin):
+    # Megjelenítendő oszlopok az admin felületen
+    list_display = ('name', 'type', 'personality', 'learning_path', 'is_active_display')
+    # Keresés a Modell neve alapján
+    search_fields = ('name',)
+    # Nem szerkeszthető mezők
+    readonly_fields = ('is_active',)
+    # Adminisztrációs műveletek hozzáadása
+    actions = ['make_active']
+
+    def is_active_display(self, obj):
+        """
+        Színes kiemelés az aktív Modellnél
+        """
+        if obj.is_active:
+            # Ha aktív, zöld színnel emeljük ki
+            return format_html('<span style="color: green; font-weight: bold;">Aktív</span>')
+        return format_html('<span style="color: gray;">Nem aktív</span>')
+
+    is_active_display.short_description = 'Aktív állapot'  # Megjelenített oszlopnév
+
+    def get_queryset(self, request):
+        """
+        A queryset módosítása, hogy az aktív Modell mindig elöl legyen
+        és a többi ABC sorrendben kövesse
+        """
+        qs = super().get_queryset(request)
+        # Az aktív Modellt tesszük az első helyre, a többit ABC sorrendben jelenítjük meg
+        return qs.order_by('-is_active', 'name')
+
+    # Egyedi admin művelet a Modell aktiválására
+    def make_active(self, request, queryset):
+        """
+        A kiválasztott Modell aktívra állítása.
+        Egyszerre csak egy Modell lehet aktív.
+        """
+        if queryset.count() > 1:
+            # Figyelmeztetés, ha több Modellt választottak ki
+            self.message_user(request, "Egyszerre csak egy Modellt aktiválhatsz.", level='error')
+            return
+        
+        # Először minden más Modellt deaktiválunk
+        Model.objects.filter(is_active=True).update(is_active=False)
+        # A kiválasztott egy Modellt aktívra állítjuk
+        queryset.update(is_active=True)
+        self.message_user(request, "A kiválasztott Modell sikeresen aktiválva lett.")
+    
+    make_active.short_description = "Kiválasztott Modell aktiválása"
+
+# A többi Modell adminisztrációja
+admin.site.register(Type)
+admin.site.register(Personality)
+admin.site.register(LearningPath)
--- adan/models.py
+++ adan/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+# Modell típusa
+class Type(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Típus neve")
+    value = models.CharField(max_length=100, verbose_name="Típus értéke")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Típus"
+        verbose_name_plural = "Típusok"
+        ordering = ['name']
+
+
+# Modell személyisége
+class Personality(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Személyiség neve")
+    value = models.CharField(max_length=100, verbose_name="Személyiség értéke")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Személyiség"
+        verbose_name_plural = "Személyiségek"
+        ordering = ['name']
+
+
+# Modell tanulási útja
+class LearningPath(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Tanulási út neve")
+    value = models.CharField(max_length=100, verbose_name="Tanulási út értéke")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Tanulási út"
+        verbose_name_plural = "Tanulási utak"
+        ordering = ['name']
+
+
+# A tényleges modell, amelyhez hozzárendelünk egy típust, személyiséget és tanulási útvonalat
+class Model(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Modell neve")
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, verbose_name="Típus")
+    personality = models.ForeignKey(Personality, on_delete=models.CASCADE, verbose_name="Személyiség")
+    learning_path = models.ForeignKey(LearningPath, on_delete=models.CASCADE, verbose_name="Tanulási út")
+    is_active = models.BooleanField(default=False, verbose_name="Aktív")  # Kapcsoló, hogy a modell aktív-e
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Modell"
+        verbose_name_plural = "Modellek"
+        ordering = ['name']
+
+    # Mielőtt mentjük a modellt, kikapcsoljuk a többi aktív modellt
+    def save(self, *args, **kwargs):
+        if self.is_active:
+            # Kikapcsoljuk a többi modellt
+            Model.objects.filter(is_active=True).update(is_active=False)
+        super().save(*args, **kwargs)
